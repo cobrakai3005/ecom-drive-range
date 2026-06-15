f6e5a66551de335a6eba63a117a198cd670df807
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2026-06-15 17:53:31 +0530

    adding config in nodemailer transporter

--- src/services/nodemailer.service.js
+++ src/services/nodemailer.service.js
@@ -1,11 +1,17 @@
 import nodemailer from "nodemailer";
-
 const transporter = nodemailer.createTransport({
-  service: "gmail", // or use 'smtp.gmail.com' with port 465
+  host: process.env.EMAIL_HOST,
+  port: parseInt(process.env.EMAIL_PORT),
+  secure: process.env.EMAIL_PORT == "465", // true for 465, false for others
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+  debug: true, // logs SMTP conversation
+  logger: true, // logs to console
 });
 
 // ------------------- Helper: Send Verification Email -------------------
