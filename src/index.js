5b3005de6fae5044a493ce2ab89f32687c7f3c24
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2026-07-02 17:32:08 +0530

    Added Two Fronetend Url

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(
       process.env.FRONTEND_URL,
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://full4-4.vercel.app",
+      process.env.FRONTEND_URL_2,
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
